dbfb139bfd9b45c1e5f8a415fc9ec8d981d2c6df
Unknown <dion_mast@hotmail.com>
2018-03-30 14:35:41 +0200

    CSS aangepast + Navigatie/Header

---
+++ HeaderNav.php
@@ -0,0 +1,21 @@
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="CSS.css" rel="stylesheet">
+<?php
+?>
+<div class="Header" name="Header">
+<!--    fix hoverover-->
+    <a href="Login.php">Logout</a>
+    <h1>Match Maker</h1>
+</div>
+<div class="Navigation">
+<nav class="navbar navbar-expand-sm navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Leerling-Match</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Profiel.php">Bedrijven</a>
+    </li>
+  </ul>
+</nav>
+</div>
