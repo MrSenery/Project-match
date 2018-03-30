606c486a9580cea6f689ae8e80809117a060ed58
Senery <sandervanosch@msn.com>
2018-03-30 15:30:39 +0100

    Matching systeem layout gedaan

--- index.php
+++ index.php
@@ -1,19 +1,128 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
   <head>
     <meta charset="UTF-8">
     <title></title>
   </head>
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="CSS.css">
+  <!--<link rel="stylesheet" href="CSS.css"> -->
   <body>
-    <?php
-    
-    ?>
+    <!-- Dit is voor het grote van het bedrijf-->
+    <div class="row">
+      <div class="col-sm-4" style="background-color:lavender;">Hoe groot wil de cursist het bedrijf hebben?</div>
+      <div class="col-sm-4" style="background-color:lavenderblush;">&nbsp;&nbsp;&nbsp;&nbsp;
+        Klein&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Middel&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Groot
+        <form action="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Grote">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Grote">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Grote">
+        </form>
+      </div>
+      <div class="col-sm-4" style="background-color:lavender;">
+        <select required name="isBelangrijk">
+          <option type="text" value="">-</option>
+          <option type="text" value="Belangrijk">Belangrijk</option>
+          <option type="text" value="Niet belangrijk">Niet belangrijk</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Dit is voor hoeveel uitdagingen de student wil hebben in het samewerking-->
+    <div class="row">
+      <div class="col-sm-4" style="background-color:lavender;">Hoeveel uitdaging wil de cursist in zijn werkzaamheden?</div>
+      <div class="col-sm-4" style="background-color:lavenderblush;">
+        Zeer weinig&nbsp;&nbsp;&nbsp;
+        Weinig&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Normaal&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Divers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Zeer divers
+        <form action="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Werkzaamheden">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Werkzaamheden">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Werkzaamheden">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Werkzaamheden">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Werkzaamheden">
+        </form>
+      </div>
+      <div class="col-sm-4" style="background-color:lavender;">
+        <select required name="isBelangrijk">
+          <option type="text" value="">-</option>
+          <option type="text" value="Belangrijk">Belangrijk</option>
+          <option type="text" value="Niet belangrijk">Niet belangrijk</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Hoe zelfstandig kan de student werken -->
+    <div class="row">
+      <div class="col-sm-4" style="background-color:lavender;">Hoe zelfstandig kan de cursist werken?</div>
+      <div class="col-sm-4" style="background-color:lavenderblush;">&nbsp;&nbsp;&nbsp;
+        Slecht&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Middel&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Goed
+        <form action="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="zelfstandigheid">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="zelfstandigheid">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="zelfstandigheid">
+        </form>
+      </div>
+      <div class="col-sm-4" style="background-color:lavender;">
+        <select required name="isBelangrijk">
+          <option type="text" value="">-</option>
+          <option type="text" value="Belangrijk">Belangrijk</option>
+          <option type="text" value="Niet belangrijk">Niet belangrijk</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Profiel 2 informatie -->
+    <div class="row">
+      <div class="col-sm-4" style="background-color:lavender;">Hoe geschikt moet het bedrijf zijn voor profiel 2 cursisten?</div>
+      <div class="col-sm-4" style="background-color:lavenderblush;">&nbsp;&nbsp;&nbsp;&nbsp;
+        N.v.t&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Matig&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Geschikt
+        <form action="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Profiel2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Profiel2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Profiel2">
+        </form>
+      </div>
+      <div class="col-sm-4" style="background-color:lavender;">
+        <select required name="isBelangrijk">
+          <option type="text" value="">-</option>
+          <option type="text" value="Belangrijk">Belangrijk</option>
+          <option type="text" value="Niet belangrijk">Niet belangrijk</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Hoe goed de student kan omgaan met klanten-->
+    <div class="row">
+      <div class="col-sm-4" style="background-color:lavender;">Hoe goed kan de cursist met klanten omgaan?</div>
+      <div class="col-sm-4" style="background-color:lavenderblush;">&nbsp;&nbsp;&nbsp;
+        Slecht&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Matig&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Normaal&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Goed&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Zeer goed
+        <form action="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Klantenomgang">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Klantenomgang">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Klantenomgang">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Klantenomgang">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="radio" name="Klantenomgang">
+        </form>
+      </div>
+      <div class="col-sm-4" style="background-color:lavender;">
+        <select required name="isBelangrijk">
+          <option type="text" value="">-</option>
+          <option type="text" value="Belangrijk">Belangrijk</option>
+          <option type="text" value="Niet belangrijk">Niet belangrijk</option>
+        </select>
+      </div>
+    </div>
   </body>
 </html>
