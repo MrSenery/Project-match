e589d49d03c2b85cc411e89f682e692707cfb1e5
Unknown <joost.loots@hotmail.nl>
2018-04-19 08:52:18 +0200

    Login-update

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
 // connection to SQL
-    mysql_connect('localhost', 'Matchmakers', '') or die (mysql_error());
-    mysql_select_database('matchmakers') or die (mysql_error());
+   $con = mysqli_connect("localhost","matchmakers","","matchmakers");
+
