e0ebddb787b8ab5c8fd02091fc82331bb4466abe
Unknown <dion_mast@hotmail.com>
2018-04-20 13:21:27 +0200

    Kleine veranderingen + BedrijfsInvoer.php

--- HeaderNav.php
+++ HeaderNav.php
@@ -22,7 +22,7 @@
       <a class="nav-link" href="index.php">Leerling-Match</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="Profiel.php">Bedrijven</a>
+      <a class="nav-link" href="BedrijfInvoer.php">Bedrijven</a>
     </li>
   </ul>
 </nav>
