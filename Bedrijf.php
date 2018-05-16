a55b2da124a8f04a7d4c79ec96f8a82f8b40a1f4
Unknown <joost.loots@hotmail.nl>
2018-05-16 08:45:05 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Bedrijf.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php include("HeaderNav.php"); ?>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Bedrijf informatie - Match Maker</title>
+  </head>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="CSS.css" rel="stylesheet">
+  <body>
+      
+  </body>
+</html>
